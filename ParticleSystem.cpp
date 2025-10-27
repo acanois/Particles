1d326e640c546ecade8f7d0f25cdb95e2252a6a0
acanois <david.richter42@gmail.com>
2025-10-27 12:50:29 -0500

    add particle system cpp

--- ParticleSystem.cpp
+++ ParticleSystem.cpp
@@ -4,4 +4,52 @@
 
 #include "ParticleSystem.h"
 
+ParticleSystem::ParticleSystem(const Vector2 position)
+        : position(position),
+          attractor(
+              std::make_unique<Attractor>(
+                  Vector2 { 1280.0f / 2.0f, 720.0f / 2.0f },
+                  0.005f
+              ))
+{
+}
 
+void ParticleSystem::addParticle()
+{
+    const auto velocityScale = 1.0f;
+    const auto startVelocity = static_cast<float>(GetRandomValue(0, 1000)) / 1000.0f;
+    const auto direction = GetRandomValue(0, 1) == 0 ? -1.0f : 1.0f;
+    const auto alpha = static_cast<unsigned char>(GetRandomValue(100, 255));
+    auto velocity = Vector2 {
+        velocityScale * startVelocity * direction,
+        0.0f
+    };
+
+    const auto letterBox = 20;
+    const auto xPos = static_cast<float>(GetRandomValue(0, 1280));
+    const auto yPos = GetRandomValue(0, 1) == 0
+                          ? letterBox
+                          : static_cast<float>(cfg.getConfig()["screenHeight"] - letterBox);
+
+    particles.push_back(
+        std::make_unique<Particle>(
+            Vector2 { xPos, yPos }, // Position
+            velocity, // Velocity
+            Vector2 { 0.0f, 0.0f }, // Acceleration
+            Color { 255, 255, 255, alpha }, // Color
+            1.0f, // Alpha
+            3.0f, // Size
+            1.0f // Mass
+        ));
+}
+
+void ParticleSystem::run() const
+{
+    for (const auto& particle: particles)
+    {
+        const auto force = attractor->attract(*particle);
+        particle->applyForce(force);
+        particle->update();
+        particle->draw();
+    }
+}
